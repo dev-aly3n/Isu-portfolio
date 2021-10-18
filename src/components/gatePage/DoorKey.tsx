bd99dea2ddaf2264c4cec6ffec19b67d842bcc91
aly3n <aly3n.dev@gmail.com>
2021-10-18 08:02:53 +0330

    door key text with background added

--- src/components/gatePage/DoorKey.tsx
+++ src/components/gatePage/DoorKey.tsx
@@ -37,7 +37,8 @@ const DoorKey: React.FC = (): JSX.Element => {
       onClick={keyClickHandler}
       className={`cycle-1 absolute left-full ml-[-15vw] top-1/2 mt-[-15vw] z-20 rounded-full w-[30vw] h-[30vw]
        flex justify-center items-center group -rotate-45 hover:rotate-0 duration-1500 hover:-translate-y-10
-       ${gateCtx.isClicked ? "rotate-0 !-translate-y-0" : ""} `}
+       hover:scale-110
+       ${gateCtx.isClicked ? "rotate-0 !-translate-y-0 !scale-100" : ""} `}
     >
       <LightKey />
       <div
@@ -47,7 +48,7 @@ const DoorKey: React.FC = (): JSX.Element => {
         <div
           className={`h-1/2 w-1/2 absolute top-[11vw] left-[11vw] mt-[-5.5vw]  ml-[-5.5vw] rounded-full
           z-50 overflow-hidden duration-1500 flex justify-center items-center rotate-45 group-hover:rotate-0 
-          ${gateCtx.isClicked ? "rotate-0" : ""}`}
+          ${gateCtx.isClicked ? "!rotate-0" : ""}`}
           style={{
             boxShadow: `${
               gateCtx.isHovered
@@ -57,7 +58,21 @@ const DoorKey: React.FC = (): JSX.Element => {
             backgroundImage: "url(/key.jpg)",
           }}
         >
-          <div className={`text-white text-5xl font-black text-center`} style={{filter:"url(#inset-shadow)"}}>open<br/>the<br/>gate</div>
+          <div
+            className={`text-transparent bg-clip-text text-4xl font-black text-center`}
+            style={{
+              textShadow: "rgba(0,0,0,0.3) 3px 1px 5px",
+              backgroundImage: "url(/keytext.jpg)",
+              backgroundPosition: "center",
+              backgroundSize: "fill",
+            }}
+          >
+            Open
+            <br />
+            The
+            <br />
+            Gate
+          </div>
         </div>
         <div className="flex flex-col absolute top-1/2 mt-[-30%] left-1/2 ml-[-47%] w-full z-40">
           <div className="relative">
