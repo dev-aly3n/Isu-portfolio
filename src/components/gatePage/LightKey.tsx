e83250a1884b7471728b85e8382c9d7e88ae2975
aly3n <aly3n.dev@gmail.com>
2021-10-27 16:12:32 +0330

    all styles transfered to the seprate css files

--- src/components/gatePage/LightKey.tsx
+++ src/components/gatePage/LightKey.tsx
@@ -11,16 +11,18 @@ const LightKey: React.FC = () => {
   const gateCtx = useContext(IsuGateCtx);
   return (
     <motion.div
-    
-      className={`w-[30vw] h-[30vw] rounded-full z-60 absolute top-1/2 left-1/2 mt-[-50%] ml-[-50%] 
-    grayscale-70 group-hover:grayscale-0 rotate-90 group-hover:rotate-0 duration-1500 
+      className={`light-key-container group-hover:grayscale-0 group-hover:rotate-0
    ${gateCtx.isClicked ? " grayscale-[0%] !rotate-[0]" : ""}`}
     >
-      <div className="relative key-door-tickness rounded-full duration-2000"
-      style={{transform:` ${gateCtx.doorAnimationFinished ? "rotateY(30deg)" : "rotateY(0deg)"} `}}
+      <div
+        style={{
+          transform: ` ${
+            gateCtx.doorAnimationFinished ? "rotateY(30deg)" : "rotateY(0deg)"
+          } `,
+        }}
       >
         <span className="key-thickness-ring"></span>
-        <img className="overflow-hidden rounded-full" src="/door1.png" alt="" />
+        <img src="/door1.png" alt="" />
         <div className="key-thickness0"></div>
         <div className="key-thickness1"></div>
         <div className="key-thickness2"></div>
