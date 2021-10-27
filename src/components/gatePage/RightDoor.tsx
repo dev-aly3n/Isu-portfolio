76598820678f03eda5ef901794eb26fe5469ea64
aly3n <aly3n.dev@gmail.com>
2021-10-27 15:42:24 +0330

    right door classes transfered

--- src/components/gatePage/RightDoor.tsx
+++ src/components/gatePage/RightDoor.tsx
@@ -1,13 +1,16 @@
+// libs
+import { motion } from "framer-motion";
+// components
 import UpLine from "./doorLines/UpLine";
 import MidLine from "./doorLines/MidLine";
 import PhLine from "./doorLines/PhLine";
 import DynamicUpLine from "./doorLines/DynamicUpLine";
 import DynamicMidLine from "./doorLines/DynamicMidLine";
 import DynamicPhLine from "./doorLines/DynamicPhLine";
-import { motion } from "framer-motion";
-
-//hooks
+// hooks
 import { useGateOpened } from "../../hooks/useGateOpened";
+// store
+// utils & animation
 import { openDoor2 } from "../../utils/animation";
 
 const LeftDoor: React.FC = (): JSX.Element => {
@@ -17,60 +20,43 @@ const LeftDoor: React.FC = (): JSX.Element => {
       variants={openDoor2}
       initial="hidden"
       animate={controls}
-      className="h-full relative right-door overflow-hidden"
-      style={{transformStyle:"preserve-3d",transformOrigin:"right"}}
+      className="right-door-container"
     >
-      <img
-        src="/gate2.jpg"
-        style={{ transform: "scaleX(1)" }}
-        className="w-full h-screen object-left"
-      />
-      <div
-        className="absolute top-0 left-0 w-36 h-[50vh] "
-        style={{ transform: "scale(-1,1)" }}
-      >
+      <img src="/gate2.jpg" />
+      <div>
         <PhLine />
       </div>
-      <div
-        className="absolute top-0 left-0 w-36 h-[50vh] "
-        style={{ transform: "scale(-1,1)" }}
-      >
+      <div>
         <DynamicPhLine />
       </div>
-      <div
-        className="absolute bottom-0 left-0 w-36 h-[50vh]"
-        style={{ transform: "scale(-1,-1)" }}
-      >
+      <div>
         <PhLine />
       </div>
-      <div
-        className="absolute bottom-0 left-0 w-36 h-[50vh]"
-        style={{ transform: "scale(-1,-1)" }}
-      >
+      <div>
         <DynamicPhLine />
       </div>
-      <div className="flex flex-col absolute top-1/2 mt-[-40%] right-0 w-full" style={{transform:"scaleX(-1)"}}>
-        <div className="relative">
+      <div>
+        <div>
           <UpLine />
-          <div className="absolute top-0 left-0 w-full h-full">
+          <div>
             <DynamicUpLine />
           </div>
         </div>
-        <div className="relative">
+        <div>
           <MidLine />
-          <div className="absolute top-0 left-0 w-full h-full">
+          <div>
             <DynamicMidLine />
           </div>
         </div>
-        <div className="relative" style={{ transform: "scaleY(-1)" }}>
+        <div style={{ transform: "scaleY(-1)" }}>
           <MidLine />
-          <div className="absolute top-0 left-0 w-full h-full">
+          <div>
             <DynamicMidLine />
           </div>
         </div>
-        <div className="relative" style={{ transform: "scaleY(-1)" }}>
+        <div style={{ transform: "scaleY(-1)" }}>
           <UpLine />
-          <div className="absolute top-0 left-0 w-full h-full">
+          <div>
             <DynamicUpLine />
           </div>
         </div>
