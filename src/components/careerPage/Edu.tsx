af0e89d665531e77a42a606c14a4ace1d01acae0
aly3n <aly3n.dev@gmail.com>
2021-11-27 22:50:01 +0330

    edu responsive

--- src/components/careerPage/Edu.tsx
+++ src/components/careerPage/Edu.tsx
@@ -15,23 +15,23 @@ interface props {
 const Edu: React.FC<props> = ({ education }) => {
   const [isExpanded, setIsExpanded] = useState(false);
   return (
-    <motion.div layout className="flex flex-col my-3">
+    <motion.div layout className="flex flex-col my-3 w-11/12 sm:w-full ">
       <div
-        className="flex justify-start items-center flex-wrap hover:cursor-pointer hover:bg-gray-500/5 duration-1000"
+        className="flex flex-col sm:flex-row justify-start items-center hover:cursor-pointer hover:bg-gray-500/5 duration-1000 relative"
         onClick={() => setIsExpanded((prevState) => !prevState)}
       >
-        <motion.span layout className="text-3xl">
+        <motion.span layout className="text-3xl absolute left-0 top-[50%] mt-[-18px] flex justify-center items-center">
           <FontAwesomeIcon
-            className={`duration-1000 ${isExpanded ? "rotate-90" : "rotate-0"}`}
+            className={`duration-500 ${isExpanded ? "rotate-90" : "rotate-0"}`}
             icon={faAngleRight}
           />{" "}
         </motion.span>
         <motion.img
           layout
           src={`/media/logos/${education.image}.png`}
-          className="block w-[10vw] ml-3"
+          className="block w-[100px] md:w-[10vw] ml-4"
         />
-        <motion.div layout className="flex flex-col ml-2">
+        <motion.div layout className="flex flex-col ml-5 sm:ml-2">
           <h3 className="text-2xl font-semibold mb-10">{education.name}</h3>
           <p>{education.field}</p>
         </motion.div>
