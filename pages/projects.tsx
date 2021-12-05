9b6083714633938d4f05c52463747e1ac0d30ac7
aly3n <aly3n.dev@gmail.com>
2021-12-05 14:07:57 +0330

    project page added

---
+++ pages/projects.tsx
@@ -0,0 +1,26 @@
+import Head from "next/head";
+import { Fragment } from "react";
+import Projects from "../src/components/projectsPage/Projects";
+
+export default function career():JSX.Element {
+  return (
+    <Fragment>
+      <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Projects - dev Aly3n - JS developer</title>
+        <meta
+          name="description"
+          content="dev Aly3n is a javascript developer especially at front-end who is also an aerospace engineer and editor-in-chief at aspad scientific magazine."
+        />
+        <meta
+          name="keywords"
+          content="dev aly3n, aly3n, aly3n developer, aly3n javascript, aly3n front-end, javascript, front-end, web development, software engineer, ali mohamadi, علی محمدی, علی محمدی برنامه نویس فرانت اند"
+        />
+        <meta name="author" content="dev aly3n" />
+      </Head>
+      <div className="z-0">
+      <Projects />
+      </div>
+    </Fragment>
+  );
+}
---
+++ src/components/projectsPage/Projects.tsx
@@ -0,0 +1,8 @@
+const Projects:React.FC = () => {
+
+    return(
+        <div></div>
+    )
+}
+
+export default Projects;
