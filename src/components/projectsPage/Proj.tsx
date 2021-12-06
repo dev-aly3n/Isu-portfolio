ca9a695c274a635a07bd5e324679c53ad6322b0a
aly3n <aly3n.dev@gmail.com>
2021-12-06 13:27:17 +0330

    add routing to projects

--- src/components/projectsPage/Proj.tsx
+++ src/components/projectsPage/Proj.tsx
@@ -1,19 +1,23 @@
 import { RefObject } from "react";
+import {motion} from 'framer-motion';
+import {useRouter} from 'next/router';
 
 interface props {
-    iframeRef:RefObject<HTMLIFrameElement>;
-    ifrExpanding:(isExpanded:boolean)=>void;
+    settingID: (id:string|null)=>void;
 }
 
-const Proj:React.FC<props> = ({iframeRef,ifrExpanding}) => {
+const Proj:React.FC<props>= ({settingID}) => {
+    const router = useRouter();
 
     const projClickHandler = () => {
         document.body.style.overflow = "hidden";
-        iframeRef.current!.src = "https://atefeh-hasani.vercel.app/";
-        ifrExpanding(true);
+        settingID("iddd")
+        setTimeout(() => {
+            router.push(`?ID=${"id"}`)
+        }, 100);
     }
     return(
-        <div className="w-96 h-[calc(100vh-160px)] flex flex-col bg-gray-50 rounded-xl overflow-hidden border-4
+        <motion.div layoutId="iframe" className="w-96 h-[calc(100vh-160px)] flex flex-col bg-gray-50 rounded-xl overflow-hidden border-4
          border-gray-300 duration-500 cursor-pointer hover:scale-105"
          onClick={projClickHandler}>
         <img src="/media/projects/wikianime.png"  />
@@ -21,7 +25,7 @@ const Proj:React.FC<props> = ({iframeRef,ifrExpanding}) => {
             <h3>portfolio website</h3>
             <p>about :Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
-    </div>
+    </motion.div>
     )
 }
 
--- src/components/projectsPage/Projects.tsx
+++ src/components/projectsPage/Projects.tsx
@@ -1,28 +1,53 @@
-import { useCallback, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import Proj from "./Proj";
+import { AnimatePresence, AnimateSharedLayout, motion } from "framer-motion";
+import { useRouter } from "next/router";
 
 const Projects: React.FC = () => {
+  const router = useRouter();
   const iframeRef = useRef<HTMLIFrameElement>(null);
-  const [iframeExpanded, setIframeExpanded] = useState(false);
-
-  const ifrExpanding = useCallback(
-    (isExpanded: boolean) => {
-      setIframeExpanded(isExpanded);
-    },
-    [setIframeExpanded]
-  );
+  const [urlID, setUrlID] = useState<null | string | string[] | undefined>(router.query.ID);
+  const settingID = useCallback(
+      (iframeID: string | null) => {
+          setUrlID(iframeID);
+        },
+        [setUrlID]
+        );
+        useEffect(() => {
+            if(window.history?.state.url.includes("projects?ID")){
+                console.log("window.history")
+            }
+    if (urlID) {
+      iframeRef.current!.src = `https://${urlID}/`;
+    }
+    console.log(urlID)
+}, [urlID]);
   return (
     <div className="flex flex-col justify-between items-center h-full w-full pb-96">
-        <div className={`fixed top-0 left-0 w-screen h-screen flex flex-col justify-between z-100
-        ${iframeExpanded ? "":"hidden"}`}>
-          <div className="h-16 w-full bg-green-900 "></div>
-          <div className="w-full h-full bg-red-500 ">
-            <iframe ref={iframeRef} src="" className="w-full h-full" />
-          </div>
+      <AnimateSharedLayout>
+        <AnimatePresence>
+          {urlID && (
+            <motion.div
+              layoutId={"iframe"}
+              className="fixed top-0 left-0 w-screen h-screen flex flex-col justify-between z-100"
+            >
+              <div
+                onClick={() => {
+                  setUrlID(null);
+                  window.history.pushState(null, "", "/projects");
+                }}
+                className="h-16 w-full bg-green-900 "
+              ></div>
+              <div className="w-full h-full bg-red-500 ">
+                <iframe ref={iframeRef} src="" className="w-full h-full" />
+              </div>
+            </motion.div>
+          )}
+        </AnimatePresence>
+        <div className="w-full h-full flex flex-wrap gap-5 justify-center items-center mt-3">
+          <Proj settingID={settingID} />
         </div>
-      <div className="w-full h-full flex flex-wrap gap-5 justify-center items-center mt-3">
-        <Proj iframeRef={iframeRef} ifrExpanding={ifrExpanding} />
-      </div>
+      </AnimateSharedLayout>
     </div>
   );
 };
