b370ac8c774f4cae32aa5d8884ecbd32e1c94494
aly3n <aly3n.dev@gmail.com>
2021-12-08 09:00:46 +0330

    project data completed

--- src/components/projectsPage/Proj.tsx
+++ src/components/projectsPage/Proj.tsx
@@ -24,21 +24,21 @@ const Proj: React.FC<props> = ({ settingID, project, projectsRef }) => {
   return (
     <motion.div
       layoutId={project.url}
-      className="w-96 min-h-[600px] flex flex-col bg-gray-50 rounded-xl overflow-hidden 
+      className="w-96 min-h-[600px] flex flex-col bg-purple-50 rounded-xl overflow-hidden 
          border-gray-300 duration-500 cursor-pointer hover:scale-105 group"
       onClick={projClickHandler}
     >
       <img className="rounded-b-xl" src={`/media/projects/${project.image}`} />
       <div className="flex flex-col mt-2 px-3">
-          <h3 className="text-2xl font-bold">{project.name}</h3>
-        <div className="flex flex-wrap gap-1">
+          <h3 className="text-2xl font-bold mb-2">{project.name}</h3>
+          <p className="text-base font-medium leading-5 mt-1">{project.desc}</p>
+        <div className="flex flex-wrap gap-1 mt-2">
           {project.techs.map((tech)=> {
-            return (<div className="px-2 py-px rounded-full text-white contrast-[0.9] font-semibold text-sm
+            return (<span key={tech.name} className="px-2 py-px rounded-full text-white contrast-[0.9] font-semibold text-sm
              duration-500 filter grayscale-[0.3] group-hover:grayscale-0"
-             style={{backgroundColor:tech.color}}>{tech.name}</div>)
+             style={{backgroundColor:tech.color}}>{tech.name}</span>)
           })}
         </div>
-        <p className="text-base font-medium leading-5 mt-1">{project.desc}</p>
       </div>
     </motion.div>
   );
