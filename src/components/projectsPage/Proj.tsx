714b86b38634949c03344f0d45d7771775092957
aly3n <aly3n.dev@gmail.com>
2021-12-08 09:05:57 +0330

    image optimised and some bug fixed

--- src/components/projectsPage/Proj.tsx
+++ src/components/projectsPage/Proj.tsx
@@ -28,14 +28,14 @@ const Proj: React.FC<props> = ({ settingID, project, projectsRef }) => {
          border-gray-300 duration-500 cursor-pointer hover:scale-105 group"
       onClick={projClickHandler}
     >
-      <img className="rounded-b-xl" src={`/media/projects/${project.image}`} />
+      <img className="rounded-b-xl shadow-2xl" src={`/media/projects/${project.image}`} />
       <div className="flex flex-col mt-2 px-3">
           <h3 className="text-2xl font-bold mb-2">{project.name}</h3>
           <p className="text-base font-medium leading-5 mt-1">{project.desc}</p>
         <div className="flex flex-wrap gap-1 mt-2">
           {project.techs.map((tech)=> {
             return (<span key={tech.name} className="px-2 py-px rounded-full text-white contrast-[0.9] font-semibold text-sm
-             duration-500 filter grayscale-[0.3] group-hover:grayscale-0"
+             duration-500 filter grayscale-[0.3] group-hover:grayscale-0 shadow-lg ring-1 ring-offset-primary-600"
              style={{backgroundColor:tech.color}}>{tech.name}</span>)
           })}
         </div>
