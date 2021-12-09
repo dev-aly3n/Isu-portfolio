983fa005ed18906eb8157e199df68bd3dfde1e30
aly3n <aly3n.dev@gmail.com>
2021-12-09 07:45:31 +0330

    useRouteHistory added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,30 @@
 import type { AppProps } from "next/app";
 import { Fragment } from "react";
-import { AnimatePresence } from "framer-motion";
-
+// libs
+import { useState,useEffect } from "react";
+import { useRouter } from "next/router";
+import { config as fontawesomeConfig } from "@fortawesome/fontawesome-svg-core";
+// components
 import Main from "../src/components/layout/Main"
 import Header from "../src/components/layout/Header";
 import Footer from "../src/components/layout/Footer";
-import "../styles/tailwind.css";
-import { config as fontawesomeConfig } from "@fortawesome/fontawesome-svg-core";
-import "@fortawesome/fontawesome-svg-core/styles.css";
 import SVGPatternsFilters from "../src/components/SVGPatternsFilters";
+// hooks
+// store
+// utils & animation
+import "../styles/tailwind.css";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+
 
 fontawesomeConfig.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    router.events.on("beforeHistoryChange", function(e){
+      console.log(e)
+    })
+  }, [])
   return (
     <Fragment>
       <Header />
