71d26cec5e093f78774e22961a89466deaf38081
aly3n <aly3n.dev@gmail.com>
2021-12-11 09:32:08 +0330

    edu css cleaned

--- src/components/careerPage/Edu.tsx
+++ src/components/careerPage/Edu.tsx
@@ -16,43 +16,31 @@ interface props {
 
 const Edu: React.FC<props> = ({ education }) => {
   const [isExpanded, setIsExpanded] = useState(false);
-  const [sec,controls] = useOnScrollAnimation();
+  const [sec, controls] = useOnScrollAnimation();
 
   return (
-    <motion.div 
-    ref={sec}
-    variants={secFading}
-    initial="hidden"
-    animate={controls}
-    
-    className="flex flex-col my-3 w-11/12 sm:w-full ">
-      <div
-        className="no-select-a-tag flex flex-col sm:flex-row justify-start items-center hover:cursor-pointer duration-1000 relative"
-        onClick={() => setIsExpanded((prevState) => !prevState)}
-      >
-        <motion.span layout className="text-3xl absolute left-0 top-[50%] mt-[-18px] flex justify-center items-center">
+    <motion.div
+      ref={sec}
+      variants={secFading}
+      initial="hidden"
+      animate={controls}
+      className="edu-container"
+    >
+      <div onClick={() => setIsExpanded((prevState) => !prevState)}>
+        <motion.span layout>
           <FontAwesomeIcon
             className={`duration-500 ${isExpanded ? "rotate-90" : "rotate-0"}`}
             icon={faAngleRight}
           />{" "}
         </motion.span>
-        <motion.img
-          layout
-          src={`/media/logos/${education.image}.png`}
-          className="block w-[100px] md:w-[10vw] ml-4"
-        />
-        <motion.div layout className="flex flex-col ml-5 sm:ml-2">
-          <h3 className="text-2xl font-semibold mb-10">{education.name}</h3>
+        <motion.img layout src={`/media/logos/${education.image}.png`} />
+        <motion.div layout>
+          <h3>{education.name}</h3>
           <p>{education.field}</p>
         </motion.div>
       </div>
-      <motion.div layout className="w-full h-0.5 bg-white" />
-      <motion.div
-        layout
-        className={`flex flex-col mx-2 my-3 px-5 overflow-hidden ${
-          isExpanded ? "h-full" : "h-0"
-        }`}
-      >
+      <motion.div layout />
+      <motion.div layout className={` ${isExpanded ? "h-full" : "h-0"}`}>
         {education.about}
       </motion.div>
     </motion.div>
--- src/components/careerPage/EduList.tsx
+++ src/components/careerPage/EduList.tsx
@@ -5,18 +5,18 @@ import { RefObject } from "react";
 import Edu from "./Edu";
 // hooks
 // store
-import {eduData} from '../../store/allData'
+import { eduData } from "../../store/allData";
 // utils & animation
 
 interface props {
-  eduRef:RefObject<HTMLDivElement>;
+  eduRef: RefObject<HTMLDivElement>;
 }
-const EduList: React.FC<props> = ({eduRef}) => {
+const EduList: React.FC<props> = ({ eduRef }) => {
   return (
-    <div  ref={eduRef}>
-        <motion.h2  layout  className="text-5xl text-gray-300 mb-10 mx-auto">Education</motion.h2>
-      {eduData.map((education)=>{
-          return <Edu key={education.name} education={education} />
+    <div ref={eduRef}>
+      <motion.h2 layout>Education</motion.h2>
+      {eduData.map((education) => {
+        return <Edu key={education.name} education={education} />;
       })}
     </div>
   );
