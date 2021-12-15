a265099a99811f47bab5c6a7d918e4d500fa3617
aly3n <aly3n.dev@gmail.com>
2021-12-15 16:36:23 +0330

    admin folder initilized

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,15 +1,16 @@
 import Head from "next/head";
 import { Fragment } from "react";
+import Admin from "../src/components/adminPage/Admin";
 
 export default function admin():JSX.Element {
   return (
     <Fragment>
       <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Projects - dev Aly3n - JS developer</title>
+        <title>Admin - dev Aly3n - JS developer</title>
       </Head>
       <div className="z-0">
-      
+      <Admin />
       </div>
     </Fragment>
   );
