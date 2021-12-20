4dad762b977a24a178b8d77a63b82dc9baed7db5
aly3n <aly3n.dev@gmail.com>
2021-12-20 18:43:05 +0330

    img replaced by next/image in contact

--- src/components/careerPage/Contact.tsx
+++ src/components/careerPage/Contact.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 interface props {
   contact: { site: string; username: string; logo: string; link: string };
 }
@@ -6,7 +8,14 @@ const Contact: React.FC<props> = ({ contact }) => {
   return (
     <div className="contact-container">
       <a href={contact.link} target="_blank">
-        <img src={`/media/logos/${contact.logo}.jpg`} />
+        <span>
+          <Image
+            alt={contact.site}
+            src={`/media/logos/${contact.logo}.jpg`}
+            layout="fill"
+            objectFit="cover"
+          />
+        </span>
         <div>
           <h4>{contact.site}</h4>
           <p>{contact.username}</p>
