aeadbf8d46c7f9b61c8ffeb0c8c2bc9f2652db56
aly3n <aly3n.dev@gmail.com>
2022-01-09 23:12:19 +0330

    header img test

---
+++ src/__tests__/__components__/__career__/__headerImg.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react";
+import HeaderImg from "../../../components/careerPage/HeaderImg";
+import React, { useRef } from "react";
+import { RefObject } from "react";
+
+const ref = new Object() as RefObject<HTMLDivElement>;
+const allRefs = {
+  bioRef: ref,
+  eduRef: ref,
+  skillsRef: ref,
+  contactRef: ref,
+};
+
+describe("career/Skill", () => {
+  it("render heads", () => {
+    render(<HeaderImg allRefs={allRefs} />);
+    const skillHead = screen.getByText("Education");
+    expect(skillHead).toBeInTheDocument();
+  });
+});
--- src/__tests__/__components__/__career__/__skills.test.tsx
+++ src/__tests__/__components__/__career__/__skills.test.tsx
@@ -7,14 +7,26 @@ import { RefObject } from "react";
 
 
 
+const ref = new Object() as RefObject<HTMLDivElement>;
 
 describe("career/Skill", () => {
 
     it("render heads", () => {
-    const ref = new Object() as RefObject<HTMLDivElement>;
 
         render(<Skills skillsRef={ref} />);
-    const skillHead = screen.getByText('Hard Skills');
+        const skillHead = screen.getByText('Hard Skills');
         expect(skillHead).toBeInTheDocument();
   });
+
+  it("rebder educational skills", ()=> {
+
+
+    render(<Skills skillsRef={ref} />);
+    const skill = screen.getByText("Soft Skills")
+    ;
+
+    expect(skill).toBeInTheDocument();
+    expect(skill).toBeValid();
+    expect(skill).toBeDefined;
+  })
 });
