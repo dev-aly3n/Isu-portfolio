a1384a3c3f220af49bdabed941497b0cef9bf24a
aly3n <aly3n.dev@gmail.com>
2022-07-10 02:37:43 +0430

    responsive recom

--- src/components/careerPage/Recommendation/Recom.tsx
+++ src/components/careerPage/Recommendation/Recom.tsx
@@ -23,13 +23,13 @@ const Recom:React.FC<props> = ({recom}) =>{
         animate={controls}
         exit="out"
         href={recom.url} target="_blank" className="h-full w-full group">
-        <div className=" flex w-full h-full bg-primary-500/10 group-hover:bg-primary-500/40 duration-300 p-5 rounded-3xl gap-5">
+        <div className="flex flex-col ssm:flex-row items-center w-full h-full bg-primary-500/10 group-hover:bg-primary-500/40 duration-300 p-5 rounded-3xl gap-5">
         <img
           src={recom.image}
           className="w-28 h-28 rounded-full"
         />
       <div className="w-5/6 flex flex-col gap-1">
-          <h4 className="text-xl font-medium">{recom.name}</h4>
+          <h4 className="text-xl font-medium text-center ssm:text-left">{recom.name}</h4>
           <h4 className="text-base font-normal">{recom.job}</h4>
         <p className="text-white/70 ">
           {recom.type}
