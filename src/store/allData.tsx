b782de254b53414f826850ca58ac2d41e893d1d9
aly3n <aly3n.dev@gmail.com>
2022-07-22 22:48:12 +0430

    change data of biography

--- src/store/allData.tsx
+++ src/store/allData.tsx
@@ -80,7 +80,7 @@ export const biochapters: { title: string; desc: React.ReactElement<any> }[] = [
       <>
         Aly started to work with{" "}
         <a
-          href="https://dedogmadao.com/"
+          href="https://demmortaltreasure.com/"
           target="_blank"
           className="link-inside-text"
         >
@@ -92,7 +92,7 @@ export const biochapters: { title: string; desc: React.ReactElement<any> }[] = [
         hook for interacting with Web3 and Ethereum chains. The big challenge
         here was implementing the{" "}
         <a
-          href="https://staging.demmortaltreasure.com/auction"
+          href="https://stagingdedogma.vercel.app/auction"
           target="_blank"
           className="link-inside-text"
         >
@@ -101,7 +101,7 @@ export const biochapters: { title: string; desc: React.ReactElement<any> }[] = [
         that needed a huge knowledge of web3 concepts, you can mint a NFT in
         this page. Another fun big challenge was implementing{" "}
         <a
-          href="https://staging.demmortaltreasure.com/gameboard"
+          href="https://stagingdedogma.vercel.app/gameboard"
           target="_blank"
           className="link-inside-text"
         >
