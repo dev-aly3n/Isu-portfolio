81521a4d29dc37d4b504ab0417e53d6da4e2c3e3
dev-aly3n <dev.aly3n@gmail.com>
2023-07-10 21:02:08 +0330

    fix

--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -3,7 +3,7 @@ const Footer: React.FC = () => {
     <div className="flex flex-col justify-center items-center pt-14 pb-1 ">
       <div className="w-[92vw] bg-gray-300 h-px" />
       <div className="text-gray-400 text-center text-xs">
-        dev-Aly3n © 2020 - 2021 All Rights Reserved.
+        dev-Aly3n © 2019 - 2023 All Rights Reserved.
       </div>
     </div>
   );
